c2d5d154b81503bba4540cb6568b46a563358711
Jackson Junior <jacksongjunior@gmail.com>
2024-04-10 21:34:11 -0300

    GET lists

--- src/main/java/com/devsuperior/dslist/entities/GameList.java
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -20,9 +20,9 @@ public class GameList {
 	public GameList() {
 	}
 	
-	public GameList(Long id, String nome) {
+	public GameList(Long id, String name) {
 		this.id = id;
-		this.name = nome;
+		this.name = name;
 	}
 
 	public Long getId() {
@@ -33,12 +33,12 @@ public class GameList {
 		this.id = id;
 	}
 
-	public String getNome() {
+	public String getName() {
 		return name;
 	}
 
-	public void setNome(String nome) {
-		this.name = nome;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	@Override
